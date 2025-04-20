68318c298550e04cdfd375a2ed2eccddb4c521cd
choi-ik <ci980704@naver.com>
2025-04-20 07:00:01 +0000

    [FLIZ-210/trainer] fix: 트레이너 도메인 빌드 에러 해결 (#132)
    * FLIZ-210 chore: 빌드에러 방지를 위해 임시 Dummy export 작성
    
    * FLIZ-210 chore: 트레이너 도메인 preRender 오류 방지를 위한 Suspense 설정과 변수 참조 에러 수정
    
    * FLIZ-210 chore: 누락된 Todo 주석 추가

--- apps/trainer/app/schedule-management/reservation/page.tsx
+++ apps/trainer/app/schedule-management/reservation/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+
 import MemberListContainer from "../_components/MemberListContainer";
 import Header from "./_components/Header";
 import ReservationAdderButton from "./_components/ReservationAdderButton";
@@ -5,7 +7,10 @@ import ReservationAdderButton from "./_components/ReservationAdderButton";
 function Reservation() {
   return (
     <main className="flex h-full flex-col">
-      <Header />
+      {/** TODO: prerender 오류 방지를 위한 임시 fallback으로 추후 수정 */}
+      <Suspense fallback={<div>preRender 오류 방지를 위한 임시 fallback</div>}>
+        <Header />
+      </Suspense>
       <MemberListContainer renderFooterReservationButton={ReservationAdderButton} />
     </main>
   );
