6b75b68eba9a5771e73522cfc07a876409c786cb
choi-ik <ci980704@naver.com>
2025-05-18 14:21:55 +0000

    [FLIZ-254/user] feat: 유저 도메인 PWA 설정 (#153)
    * FLIZ-254 chore: next-pwa install
    
    * FLIZ-254 chore: next-pwa next.config.mjs에 설정
    
    * FLIZ-254 chore: FitLink 이미지 아이콘 등록
    
    * FLIZ-254 chore: manifest 설정
    
    * FLIZ-254 chore: PWA 빌드시 생성되는 파일들 gitignore에 추가
    
    * FLIZ-254 chore: PWAManifestLink 컴포넌트 구현 및 layout에 주입

--- apps/user/app/layout.tsx
+++ apps/user/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Viewport } from "next";
 
 import "./global.css";
+import PWAManifestLinks from "@user/PWAManifestLinks";
+
 import Providers from "@user/components/Providers";
 
 export const viewport: Viewport = {
@@ -16,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <PWAManifestLinks />
+      </head>
       <body className="bg-background-primary text-text-primary md:max-w-mobile relative box-content flex h-screen min-h-screen w-full flex-col md:mx-auto md:overflow-x-hidden md:shadow-lg">
         <Providers>{children}</Providers>
       </body>
--- apps/user/next.config.mjs
+++ apps/user/next.config.mjs
@@ -1,8 +1,22 @@
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+import withPWA from "next-pwa";
+
+// eslint-disable-next-line no-undef
+const isProd = process.env.NODE_ENV === "production";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const pwaConfig = withPWA({
+  dest: "public",
+  disable: !isProd,
+  runtimeCaching: [],
+  register: true,
+  skipWaiting: true,
+  clientsClaim: true,
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,4 +32,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default pwaConfig(nextConfig);
