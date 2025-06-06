1d6af7ea7ec4e5e137158f00e3adb1031c37ab7a
choi-ik <ci980704@naver.com>
2025-06-06 13:16:25 +0000

    [FLIZ-327] fix: 휴무일 삭제 시 다이얼로그 안 닫히는 버그 및 오류 시 토스트 메시지 작업 추가 (#199)
    * FLIZ-327 fix: 휴무일 삭제시 다이얼로그 안 닫히는 버그 및 오류 시 토스트 메시지 작업 추가
    
    * FLIZ-327 chore: 불핋요한 테스트 코드 제거
    
    * FLIZ-327 fix: 불필요한 콘솔 로그 제거

--- apps/trainer/app/my-page/_components/DeleteMyDayOffDialog/index.tsx
+++ apps/trainer/app/my-page/_components/DeleteMyDayOffDialog/index.tsx
@@ -5,10 +5,11 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogFooter,
+  DialogTitle,
 } from "@ui/components/Dialog";
 import React from "react";
+import { toast } from "react-toastify";
 
 import { myInformationQueries } from "@trainer/queries/myInformation";
 
@@ -26,58 +27,57 @@ type DeleteMyDayOffDialogProps = {
 export default function DeleteMyDayOffDialog({
   deleteDayOffData,
   open,
+  onOpenChange,
 }: DeleteMyDayOffDialogProps) {
   const queryClient = useQueryClient();
 
-  const { mutate, isSuccess, isError } = useMutation({
+  const { mutate } = useMutation({
     mutationFn: (params: {
       requestPath: { dayOffId: number };
       requestBody: DeleteTimeOffRequestBody;
     }) => deleteTimeOff(params.requestPath, params.requestBody),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: myInformationQueries.dayOff().queryKey });
+      onOpenChange(false);
+    },
+    onError: () => {
+      toast.error("삭제 중 오류가 발생했습니다.");
     },
   });
 
   const handleDeleteDayOff = () => {
     if (!deleteDayOffData) return;
 
-    mutate(
-      {
-        requestPath: { dayOffId: deleteDayOffData.dayOffId },
-        requestBody: {
-          dayOfWeek: deleteDayOffData.dayOfWeek,
-          dayOfTime: deleteDayOffData.dayOffDate,
-        },
+    mutate({
+      requestPath: { dayOffId: deleteDayOffData.dayOffId },
+      requestBody: {
+        dayOfWeek: deleteDayOffData.dayOfWeek,
+        dayOfTime: deleteDayOffData.dayOffDate,
       },
-      {
-        onSuccess: () => {
-          if (isSuccess) {
-            console.log("성공적으로 삭제되었습니다.");
-          }
-        },
-        onError: (error) => {
-          if (isError) {
-            console.error("삭제 중 오류가 발생했습니다:", error);
-          }
-        },
-      },
-    );
+    });
+  };
+
+  const handleClickCloseDialog = () => {
+    onOpenChange(false);
   };
 
   return (
     <Dialog open={open}>
       <DialogContent>
-        {/* <DialogTitle className="w-full text-center">휴무일을 삭제하시겠습니까?</DialogTitle> */}
-        <DialogDescription className="text-headline text-text-primary w-full text-center">
-          휴무일을 삭제하시겠습니까?
-        </DialogDescription>
+        <DialogTitle className="w-full text-center">휴무일을 삭제하시겠습니까?</DialogTitle>
+
         <DialogFooter>
-          <Button className="flex w-full flex-1" variant="brand" onClick={handleDeleteDayOff}>
-            삭제
-          </Button>
           <DialogClose asChild>
-            <Button className="flex w-full flex-1" variant="outline">
+            <Button className="flex w-full flex-1" variant="brand" onClick={handleDeleteDayOff}>
+              삭제
+            </Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button
+              className="flex w-full flex-1"
+              variant="outline"
+              onClick={handleClickCloseDialog}
+            >
               취소
             </Button>
           </DialogClose>
